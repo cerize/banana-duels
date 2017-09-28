9e6235f0df58bf0f95ccf636709bfb607aad02cd
Josh Lim <josh.lim@bananatag.com>
2017-09-28 15:28:00 -0700

    Add base login and user routes

--- server.js
+++ server.js
@@ -5,11 +5,48 @@ const Hapi = require('hapi');
 const server = new Hapi.Server();
 server.connection({ port: 3000, host: 'localhost'  });
 
+const userDb = {};
+
+const _addUser = (newUser, userDb) => {
+    userDb[newUser.email] = newUser
+};
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: function (request, reply) {
+        reply('Hello, world!');
+    }
+});
+
+server.route({
+    method: 'POST',
+    path: '/login',
+    handler: function (request, reply) {
+        _addUser(request.payload, userDb)
+        reply();
+    }
+});
+
+server.route({
+    method: 'GET',
+    path: '/users',
+    handler: function (request, reply) {
+        reply(userDb);
+    }
+});
+
 server.start((err) => {
-        if (err) {
-                throw err;
-        }
-        console.log(`Server running at: ${server.info.uri}`);
+    if (err) {
+        throw err;
+    }
+    console.log(`Server running at: ${server.info.uri}`);
+
+    startGame(
+        choosePlayers,
+        waitForRequest,
+        sendResults,
+        startGame
+    );
 
 });
 
